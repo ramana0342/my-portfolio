b119842724238e8fa26a12dce81336e6cc02f308
Ramana Reddy <ramanareddy@promilo.com>
2026-05-01 14:57:17 +0530

    Mailer Fix

--- back-end/config/mailer.js
+++ back-end/config/mailer.js
@@ -1,9 +1,16 @@
 import nodemailer from "nodemailer";
+import dns from "dns";
 
+dns.setDefaultResultOrder("ipv4first"); 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,         
+  secure: false,      
   auth: {
     user: process.env.APP_EMAIL,
     pass: process.env.APP_EMAIL_PASS,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 15000,
 });
