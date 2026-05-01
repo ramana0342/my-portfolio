99368f58ea8aab5b5c36a93e51ff41b28516e42c
Ramana Reddy <ramanareddy@promilo.com>
2026-05-01 11:07:02 +0530

    Mailer Fix

--- back-end/config/mailer.js
+++ back-end/config/mailer.js
@@ -1,24 +1,9 @@
 import nodemailer from "nodemailer";
-import dns from "dns";
-
-dns.setDefaultResultOrder("ipv4first");
 
 export const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  family: 4, // ✅ force IPv4 (very important)
+  service: "gmail",
   auth: {
     user: process.env.APP_EMAIL,
     pass: process.env.APP_EMAIL_PASS,
-  },
-  pool: true,
-  maxConnections: 5,
-  maxMessages: 100,
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
-  tls: {
-    rejectUnauthorized: false,
   },
 });
