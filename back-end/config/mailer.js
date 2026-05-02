ff196c80951cb81542b18782c3ccca11e5d6fa28
Ramana Reddy <ramanareddy@promilo.com>
2026-05-02 07:26:04 +0530

    Mailer Fix

--- back-end/config/mailer.js
+++ back-end/config/mailer.js
@@ -8,13 +8,18 @@ import nodemailer from "nodemailer";
 //   },
 // });
 
+
+
 export const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // use STARTTLS
-  family: 4,     // force IPv4 — fixes ENETUNREACH
-  auth: {
+host: "smtp.gmail.com",
+port: 587,
+secure: false, 
+requireTLS: true,
+logger: true,
+debug: true,
+auth: {
     user: process.env.APP_EMAIL,
-    pass: process.env.APP_EMAIL_PASS,
-  },
+    pass: process.env.APP_EMAIL_PASS
+},
+
 });
