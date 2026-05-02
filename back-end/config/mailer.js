a3da7000a34cf655b027ee1aa603f21b05fc1fef
Ramana Reddy <ramanareddy@promilo.com>
2026-05-02 09:53:15 +0530

    Mailer Fixed

--- back-end/config/mailer.js
+++ back-end/config/mailer.js
@@ -1,43 +1,39 @@
 import { google } from 'googleapis';
 
 const oAuth2Client = new google.auth.OAuth2(
-  process.env.GOOGLE_CLOUD_CLIENT_ID,
-  process.env.GOOGLE_CLOUD_CLIENT_SECRET,
-  "https://developers.google.com/oauthplayground"
+    process.env.GOOGLE_CLOUD_CLIENT_ID,
+    process.env.GOOGLE_CLOUD_CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
 );
 
 oAuth2Client.setCredentials({
-  refresh_token: process.env.GOOGLE_CLOUD_REFRESH_TOKEN
+    refresh_token: process.env.GOOGLE_CLOUD_REFRESH_TOKEN
 });
 
 const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
 
-/**
- * COMMON SEND FUNCTION
- * This replaces transporter.sendMail and works on Render!
- */
 export const commonEmailSender = async ({ to, subject, html, replyTo }) => {
-  const utf8Subject = `=?utf-8?B?${Buffer.from(subject).toString('base64')}?=`;
-  const messageParts = [
-    `From: Ramana Portfolio <${process.env.APP_EMAIL}>`,
-    `To: ${to}`,
-    replyTo ? `Reply-To: ${replyTo}` : '',
-    `Content-Type: text/html; charset=utf-8`,
-    `Mime-Version: 1.0`,
-    `Subject: ${utf8Subject}`,
-    '',
-    html,
-  ];
+    const utf8Subject = `=?utf-8?B?${Buffer.from(subject).toString('base64')}?=`;
+    const messageParts = [
+        `From: Ramana Portfolio <${process.env.APP_EMAIL}>`,
+        `To: ${to}`,
+        replyTo ? `Reply-To: ${replyTo}` : '',
+        `Content-Type: text/html; charset=utf-8`,
+        `Mime-Version: 1.0`,
+        `Subject: ${utf8Subject}`,
+        '',
+        html,
+    ];
 
-  const message = messageParts.filter(line => line !== '').join('\n');
-  const encodedMessage = Buffer.from(message)
-    .toString('base64')
-    .replace(/\+/g, '-')
-    .replace(/\//g, '_')
-    .replace(/=+$/, '');
+    const message = messageParts.filter(line => line !== '').join('\n');
+    const encodedMessage = Buffer.from(message)
+        .toString('base64')
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
 
-  return await gmail.users.messages.send({
-    userId: 'me',
-    requestBody: { raw: encodedMessage },
-  });
+    return await gmail.users.messages.send({
+        userId: 'me',
+        requestBody: { raw: encodedMessage },
+    });
 };
